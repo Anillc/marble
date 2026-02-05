e910072810c6a47400d052ecc31255e9bf6843a1
Anillc <i@anillc.cn>
2026-02-05 21:07:22 +0800

    fix ota

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
         nativeBuildInputs = with pkgs; [];
       };
     };
-    flake.robotnixConfigurations.default = robotnix.lib.robotnixSystem {
+    flake.robotnixConfigurations.default = robotnix.lib.robotnixSystem ({ pkgs, ... }: {
       flavor = "lineageos";
       device = "marble";
       flavorVersion = "23.0";
@@ -31,7 +31,22 @@
         url = "https://hydra.ani.llc/job/marble/marble/packages.x86_64-linux.default/latest/download/1/marble-otaDir/";
       };
       microg.enable = true;
-    };
+
+      # workaround for https://github.com/nix-community/robotnix/issues/354
+      product.extraConfig = "PRODUCT_PACKAGE_OVERLAYS += anillc/overlay";
+      source.dirs."anillc/overlay".src = let
+        url = "https://hydra.ani.llc/job/marble/marble/packages.x86_64-linux.default/latest/download/1/marble-otaDir/lineageos-marble.json";
+      in pkgs.runCommand "overlay" {} ''
+        VALUES=$out/packages/apps/Updater/app/src/main/res/values
+        mkdir -p $VALUES
+        cat > $VALUES/strings.xml << EOF
+        <?xml version="1.0" encoding="utf-8"?>
+        <resources>
+            <string name="updater_server_url">${url}</string>
+        </resources>
+        EOF
+      '';
+    });
     flake.hydraJobs = { inherit (self) packages; };
   };
 }
